a3ddf667a7266f04c7873aae49438da944657e03
qmraphael <QM.Praktikant1@almi.at>
2024-05-13 13:12:20 +0200

    13.5 | Workshop Requirements

--- frontend-fachbeate/src/app/models/workshop-requirement.ts
+++ frontend-fachbeate/src/app/models/workshop-requirement.ts
@@ -3,22 +3,37 @@ import { TechnologistAppointment } from "./technologist-appointment";
 export interface WorkshopRequirement extends TechnologistAppointment {
     subject: string;
     company: string;
-    amountParticipants: number;
-    travelFrom: string;
-    travelType: string;
-    language: string;
-    shouldBeTranslated: boolean;
-    seminarPresenter: string;
-    hotelBooking: boolean;
-    locationAndDesiredPlace: string;
-    locationFromDate: Date;
-    locationToDate: Date;
-    amountSingleRooms: number;
-    amountDoubleRooms: number;
-    flightBooking: boolean;
-    flightHereDateTime: Date;
-    flightReturnDateTime: Date;
-    flightFrom: string;
-    flightTo: string;
-    otherTravelRequests: string;
+    amountParticipants?: number;
+    travelFrom?: string;
+    travelType?: string;
+    language?: string;
+    shouldBeTranslated?: boolean;
+    seminarPresenter?: string;
+
+    hotelBooking?: boolean;
+    locationAndDesiredPlace?: string;
+    locationFromDate?: Date;
+    locationToDate?: Date;
+    amountSingleRooms?: number;
+    amountDoubleRooms?: number;
+
+    flightBooking?: boolean;
+    flightHereDateTime?: Date;
+    flightReturnDateTime?: Date;
+    flightFrom?: string;
+    flightTo?: string;
+    otherTravelRequests?: string;
+
+    trip?: boolean;
+    tripDateTime?: Date;
+    tripLocation?: string;
+    otherTripRequests?: string;
+
+    meal?: boolean;
+    mealAmount?: number;
+    mealDateTime?: Date;
+    mealWishes?: number[];
+    customerPresent?: boolean;
+    diploma?: boolean;
+    otherRequests?: string;
 }
