30c236c1cc3f0a4437c0103f8ea71a6f3db2139f
QmChri <QM.Praktikant2@almi.at>
2024-05-29 08:25:14 +0200

    29.5 | newDate popup

--- frontend-fachbeate/src/app/components/contents/new-date-entry/new-date-entry.component.ts
+++ frontend-fachbeate/src/app/components/contents/new-date-entry/new-date-entry.component.ts
@@ -7,20 +7,37 @@ import { AbschlussBerichtComponent } from '../abschluss-bericht/abschluss-berich
   templateUrl: './new-date-entry.component.html',
   styleUrl: './new-date-entry.component.scss'
 })
-export class NewDateEntryComponent implements OnInit{
+export class NewDateEntryComponent implements OnInit {
 
+  addItem: string = "";
+  reasonSelect: number = 0;
+  reasons: string[] = ['Urlaub',
+    'Zeitausgleich',
+    'Vorläufige Kundenreservierung',
+    'Seminar',
+    'Messe',
+    'HomeOffice',
+    'Haus Oftering',
+    'Kundenbesuch'];
 
-  inputDate: {start?: string, end?: string, text?: String} = {}
+  inputDate: { start?: string, end?: string, text?: String } = {}
 
-  constructor(public dialogRef: MatDialogRef<AbschlussBerichtComponent>, @Inject(MAT_DIALOG_DATA) public timeSpan: {start: string, end: string}){
+  constructor(public dialogRef: MatDialogRef<AbschlussBerichtComponent>, @Inject(MAT_DIALOG_DATA) public timeSpan: { start: string, end: string }) {
   }
   ngOnInit(): void {
     this.inputDate.start = this.timeSpan.start;
     this.inputDate.end = this.timeSpan.end;
   }
 
-  test(){
+  addToList(addItem: string){
+    this.reasons.push(addItem);
+  }
+
+  test() {
     console.log(this.inputDate)
+  }
+  closeDialog(save: boolean) {
+    this.dialogRef.close({ dialogRef: this.dialogRef, save: save });
   }
 
 }
--- frontend-fachbeate/src/app/components/dashboard/dashboard.component.ts
+++ frontend-fachbeate/src/app/components/dashboard/dashboard.component.ts
@@ -15,7 +15,6 @@ import { NewDateEntryComponent } from '../contents/new-date-entry/new-date-entry
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent implements OnInit {
-
   calendarOptions: CalendarOptions = {
     initialView: 'dayGridMonth',
     plugins: [dayGridPlugin, interactionPlugin],
@@ -25,28 +24,28 @@ export class DashboardComponent implements OnInit {
     select: (arg) => this.handleSelect(arg),
     events: [],
     firstDay: 1,
-    
+
   };
 
-  constructor(private http: HttpService,private router: Router, private dialog: MatDialog){
+  constructor(private http: HttpService, private router: Router, private dialog: MatDialog) {
 
   }
 
   ngOnInit(): void {
     this.loadEvents();
   }
-  
-  loadEvents(){
+
+  loadEvents() {
     this.http.getCustomerRequirements().subscribe({
-      next: data => { 
+      next: data => {
         this.calendarOptions.events = data.map(value => ({
-          id: ""+value.id,
+          id: "" + value.id,
           title: value.requestedTechnologist!.firstName + " " + value.requestedTechnologist!.lastName + " - " + value.company,
           start: value.startDate,
           end: value.endDate,
           backgroundColor: value.requestedTechnologist!.color,
           borderColor: value.requestedTechnologist!.color,
-      }));
+        }));
       },
       error: err => {
         console.log(err);
@@ -54,23 +53,23 @@ export class DashboardComponent implements OnInit {
     })
   }
 
-  handleSelect(clickInfo: any){
+  handleSelect(clickInfo: any) {
     console.log(clickInfo);
-    
-    this.openDialog({start: clickInfo.startStr, end: clickInfo.endStr})
+
+    this.openDialog({ start: clickInfo.startStr, end: clickInfo.endStr })
   }
 
   handleEventClick(clickInfo: any): void {
     this.router.navigate(['/customer-requirements', clickInfo.event.id]);
   }
 
-  
-  openDialog(timeSpan: {start: string, end: string}) {
+
+  openDialog(timeSpan: { start: string, end: string }) {
     const dialogRef = this.dialog.open(NewDateEntryComponent, {
-      height: '40rem',
-      width: '60rem',
+      height: '26rem',
+      width: '25rem',
       data: timeSpan
     });
   }
-  
+
 }
