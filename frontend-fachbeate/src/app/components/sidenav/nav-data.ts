9b94cbd89e77cfd162884085ccc43e4ead999bb3
qmraphael <QM.Praktikant1@almi.at>
2024-05-29 13:34:54 +0200

    29.5 | Handels hockn

--- frontend-fachbeate/src/app/components/sidenav/nav-data.ts
+++ frontend-fachbeate/src/app/components/sidenav/nav-data.ts
@@ -4,26 +4,4 @@ export const navbarData: INavbarData[] = [
     {
         routeLink: 'dashboard',
         icon: 'far fa-calendar-alt',
-        label: 'Terminübersicht'
-    },
-    {
-        routeLink: 'mainList',
-        icon: 'fal fa-list',
-        label: 'Terminliste'
-    },
-    {
-        routeLink: 'customer-requirements',
-        icon: 'fas fa-envelope-open-text',
-        label: 'Anforderungen Technologische Unterstützung'
-    },
-    {
-        routeLink: 'visitorRegistration',
-        icon: 'fal fa-users',
-        label: 'Besucheranmeldung'
-    },
-    {
-        routeLink: 'seminar-registration',
-        icon: 'fal fa-book',
-        label: 'Seminar Anmeldung'
-    }
-];
+        label: 'Terminübersic
