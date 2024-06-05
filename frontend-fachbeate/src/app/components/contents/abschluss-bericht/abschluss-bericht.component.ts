bcc79ea598417da2b0ac66d1308f6f31d72d4cc6
qmraphael <QM.Praktikant1@almi.at>
2024-06-05 08:19:03 +0200

    5.6 | Abschlussbericht ReasonSelect Fix

--- frontend-fachbeate/src/app/components/contents/abschluss-bericht/abschluss-bericht.component.ts
+++ frontend-fachbeate/src/app/components/contents/abschluss-bericht/abschluss-bericht.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { FinalReport } from '../../../models/final-report';
+import { ReasonReport } from '../../../models/reason-report';
 
 @Component({
   selector: 'app-abschluss-bericht',
@@ -12,12 +13,25 @@ export class AbschlussBerichtComponent {
   inputFinalReport: FinalReport = {}
 
   reasonSelect: number[] = []
-  buttonSelect: string[] = []
+  
+  changeSelections(event: any) {
+    var newReasonReports: ReasonReport[] = [];
 
-  changeSelections(event: any, section: number) {
-    this.buttonSelect = (section === 0) ? this.buttonSelect.filter(number => Number(number) >= 6 && Number(number) <= 7) : this.buttonSelect.filter(number => Number(number) >= 1 && Number(number) <= 5);
-    this.buttonSelect = [...this.buttonSelect, ...event.value]
+    this.reasonSelect = event.value;
+
+    event.value.forEach((element: number) => {
+      var r: ReasonReport = this.inputFinalReport.reasonReports!.find(p => p.reason === element)!
+      if(r !== null && r !== undefined){
+        newReasonReports = [...newReasonReports, r]
+      }else{
+        newReasonReports = [...newReasonReports, {reason: element}]
+      }
+    })
+    this.inputFinalReport.reasonReports = newReasonReports;
+    
   }
+
+
   constructor(
     public dialogRef: MatDialogRef<AbschlussBerichtComponent>,
     @Inject(MAT_DIALOG_DATA) public finalReport: FinalReport
@@ -25,13 +39,12 @@ export class AbschlussBerichtComponent {
     this.inputFinalReport = finalReport;
 
     if (finalReport.reasonReports !== undefined) {
+
       this.inputFinalReport.reasonReports = this.inputFinalReport.reasonReports!.filter(element => element.reason !== 0);
+
       this.reasonSelect = this.inputFinalReport.reasonReports!.map(element => element.reason)
         .filter((reason): reason is number => reason !== undefined);
-      ;
-
-      console.log("resonselect")
-      console.log(this.reasonSelect);
+      
     }
 
   }
