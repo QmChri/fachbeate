e286da3c3ebbaa65b7c6b5e61362c796aa0f488a
QmChri <QM.Praktikant2@almi.at>
2024-06-24 09:07:36 +0200

    24.6 | environment.develop

--- frontend-fachbeate/src/environments/environment.development.ts
+++ frontend-fachbeate/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  backendApi: "http://localhost:8079/",
+  backendApi: "http://angular02.almi.at:8181/",
   keycloak: {
     issuer: 'http://keycloak01.almi.at:8080/',
     realm: 'fachbeate',
