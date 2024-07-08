fcab9b0f4c7ed5b4a69c7bf19204302b6239f530
QmChri <QM.Praktikant2@almi.at>
2024-07-08 10:53:55 +0200

    Tabellenänderung Übersicht-Anforderungen #9

--- frontend-fachbeate/src/app/components/main-list/main-list.component.ts
+++ frontend-fachbeate/src/app/components/main-list/main-list.component.ts
@@ -20,11 +20,11 @@ export class MainListComponent implements OnInit {
 
   listOfColumn: ColumnDefinition[] = [
     {
-      name: 'company_name',
+      name: 'id',
       sortOrder: null,
-      sortFn: (a: DataItem, b: DataItem) => a.name!.toString().localeCompare(b.name!.toString()),
+      sortFn: (a: DataItem, b: DataItem) => 1,
       listOfFilter: [],
-      filterFn: (list: string[], item: DataItem) => list.some(a => item.name!.indexOf(a) !== -1)
+      filterFn: (list: string[], item: DataItem) => true
     },
     {
       name: 'creation_date',
@@ -89,13 +89,6 @@ export class MainListComponent implements OnInit {
       listOfFilter: [],
       filterFn: (list: string[], item: DataItem) => list.some(name => item.type!.valueOf().toString().indexOf(name.valueOf().toString()) !== -1)
     },
-    {
-      name: 'id',
-      sortOrder: null,
-      sortFn: (a: DataItem, b: DataItem) => 1,
-      listOfFilter: [],
-      filterFn: (list: string[], item: DataItem) => true
-    }
   ];
 
   constructor(public translate: TranslateService, private router: Router, private http: HttpService, private notificationService: NotificationService, public roleService: RoleService) { }
@@ -231,10 +224,10 @@ export class MainListComponent implements OnInit {
           });
 
           this.listOfDisplayData = [...this.listOfDisplayData, {
-            id: element.id!,
+            id: "F_"+element.id!,
             name: element.company?.name!,
             dateOfCreation: element.dateOfCreation !== undefined ? element.dateOfCreation : new Date(),
-            customerOrCompany: "<Leer>",
+            customerOrCompany: element.creator,
             status: (element.releaseSupervisor && element.releaseManagement)?"Freigegeben":"Nicht-Freigegeben",
             vertreter: element.representative?.firstName! + " " + element.representative?.lastName!,
             fachberater: element.requestedTechnologist?.firstName! + " " + element.requestedTechnologist?.lastName!,
@@ -242,7 +235,7 @@ export class MainListComponent implements OnInit {
               start: element.startDate,
               end: element.endDate
             },
-            customer: "<Leer>",
+            customer: (element.customerVisits[0])?element.customerVisits[0].companyName!:"<leer>",
             abschlussbericht: cntFinalReports + "/" + element.customerVisits.length,
             type: 0,
             visible: element.showUser!
@@ -266,7 +259,7 @@ export class MainListComponent implements OnInit {
           }
 
           this.listOfDisplayData = [...this.listOfDisplayData, {
-            id: element.id!,
+            id: "S_"+element.id!,
             name: "<Leer>",
             dateOfCreation: element.dateOfCreation !== undefined ? element.dateOfCreation : new Date(),
             customerOrCompany: "<Leer>",
@@ -295,7 +288,7 @@ export class MainListComponent implements OnInit {
       next: data => {
         data.forEach(element => {
           this.listOfDisplayData = [...this.listOfDisplayData, {
-            id: element.id!,
+            id: "B_" + element.id!,
             name: element.name!,
             dateOfCreation: element.dateOfCreation !== undefined ? element.dateOfCreation : new Date(),
             customerOrCompany: element.customerOrCompany!,
@@ -306,7 +299,7 @@ export class MainListComponent implements OnInit {
               start: element.fromDate,
               end: element.toDate
             },
-            customer: "<Leer>",
+            customer: (element.customerOrCompany)?element.customerOrCompany:"<leer>",
             abschlussbericht: "<Leer>",
             type: 2,
             visible: element.showUser!
@@ -326,14 +319,14 @@ export class MainListComponent implements OnInit {
     })
   }
 
-  openCRC(data: any, id: number, type: number) {
+  openCRC(data: any, id: string, type: number) {
     if (data.visible) {
       if (type === 0) {
-        this.router.navigate(['/customer-requirements', id]);
+        this.router.navigate(['/customer-requirements', id.split("_")[1]]);
       } else if (type === 1) {
-        this.router.navigate(['/seminar-registration', id]);
+        this.router.navigate(['/seminar-registration', id.split("_")[1]]);
       } else if (type === 2) {
-        this.router.navigate(['/visitorRegistration', id]);
+        this.router.navigate(['/visitorRegistration', id.split("_")[1]]);
       }
     }
   }
@@ -344,7 +337,7 @@ export class MainListComponent implements OnInit {
       this.notificationService.createBasicNotification(2, translatedMessage, '', 'topRight');
     });
     this.getNzFilters();
-    //this.loadDataPerUser();
+    this.loadDataPerUser();
     //this.tmpinitData();
     this.listOfColumn.forEach(item => {
       item.sortOrder = null;
@@ -379,176 +372,10 @@ export class MainListComponent implements OnInit {
       data.visible = false
     }
   }
-
-  /* tmpinitData() {
-     this.listOfDisplayData = [
-       {
-         id: 1,
-         name: "Project A - Long Name Test",
-         dateOfCreation: new Date("2023-06-01"),
-         customerOrCompany: "Example GmbH - Long Name Test",
-         status: "true",
-         vertreter: "B",
-         fachberater: "C",
-         timespan: {
-           start: new Date("2023-06-01"),
-           end: new Date("2023-06-30"),
-         },
-         customer: "A",
-         abschlussbericht: "Fertig",
-         type: 1,
-       },
-       {
-         id: 2,
-         name: "Project B - Testing Long Names",
-         dateOfCreation: new Date("2023-06-02"),
-         customerOrCompany: "Demo AG - Example Long Company Name",
-         status: "false",
-         vertreter: "C",
-         fachberater: "A",
-         timespan: {
-           start: new Date("2023-07-10"),
-           end: new Date("2023-07-31"),
-         },
-         customer: "F",
-         abschlussbericht: "In Arbeit",
-         type: 2,
-       },
-       {
-         id: 3,
-         name: "Project C - Another Long Test Name",
-         dateOfCreation: new Date("2023-06-03"),
-         customerOrCompany: "Test GmbH - Long Customer Name for Testing",
-         status: "true",
-         vertreter: "Hans Schmidt",
-         fachberater: "Sabine Fischer",
-         timespan: {
-           start: new Date("2023-08-05"),
-           end: new Date("2023-08-20"),
-         },
-         customer: "TEster langer Test",
-         abschlussbericht: "Fertig",
-         type: 0,
-       },
-       // Additional test data
-       {
-         id: 4,
-         name: "Project D - Extended Name for Testing",
-         dateOfCreation: new Date("2023-06-04"),
-         customerOrCompany: "Alpha Ltd. - Long Customer Company Name",
-         status: "true",
-         vertreter: "D",
-         fachberater: "E",
-         timespan: {
-           start: new Date("2023-09-01"),
-           end: new Date("2023-09-15"),
-         },
-         customer: "C",
-         abschlussbericht: "Fertig",
-         type: 1,
-       },
-       {
-         id: 5,
-         name: "Project E - Long Name Test for Data",
-         dateOfCreation: new Date("2023-06-05"),
-         customerOrCompany: "Beta Corp. - Test Company Name for Long Data",
-         status: "false",
-         vertreter: "E",
-         fachberater: "F",
-         timespan: {
-           start: new Date("2023-10-01"),
-           end: new Date("2023-10-10"),
-         },
-         customer: "D",
-         abschlussbericht: "Nicht gestartet",
-         type: 0,
-       },
-       {
-         id: 6,
-         name: "Project F - More Test Data with Long Names",
-         dateOfCreation: new Date("2023-06-06"),
-         customerOrCompany: "Gamma Inc. - Long Company Name for Testing",
-         status: "true",
-         vertreter: "F",
-         fachberater: "G",
-         timespan: {
-           start: new Date("2023-11-01"),
-           end: new Date("2023-11-20"),
-         },
-         customer: "E",
-         abschlussbericht: "In Arbeit",
-         type: 2,
-       },
-       {
-         id: 7,
-         name: "Project G - Final Test with Long Names",
-         dateOfCreation: new Date("2023-06-07"),
-         customerOrCompany: "Delta LLC - Long Name for Delta Company",
-         status: "false",
-         vertreter: "G",
-         fachberater: "H",
-         timespan: {
-           start: new Date("2023-12-01"),
-           end: new Date("2023-12-15"),
-         },
-         customer: "F",
-         abschlussbericht: "Fertig",
-         type: 1,
-       },
-       {
-         id: 8,
-         name: "Project H - Large Scale Testing Names",
-         dateOfCreation: new Date("2023-06-08"),
-         customerOrCompany: "Epsilon GmbH - Large Customer Name for Testing",
-         status: "true",
-         vertreter: "H",
-         fachberater: "I",
-         timespan: {
-           start: new Date("2023-12-20"),
-           end: new Date("2023-12-31"),
-         },
-         customer: "G",
-         abschlussbericht: "Fertig",
-         type: 0,
-       },
-       {
-         id: 9,
-         name: "Project I - Extensive Data with Long Names",
-         dateOfCreation: new Date("2023-06-09"),
-         customerOrCompany: "Zeta AG - Comprehensive Testing Name for Customer",
-         status: "false",
-         vertreter: "I",
-         fachberater: "J",
-         timespan: {
-           start: new Date("2024-01-01"),
-           end: new Date("2024-01-15"),
-         },
-         customer: "H",
-         abschlussbericht: "In Arbeit",
-         type: 2,
-       },
-       {
-         id: 10,
-         name: "Project J - Advanced Testing for Large Names",
-         dateOfCreation: new Date("2023-06-10"),
-         customerOrCompany: "Theta Corp. - Testing Company for Large Names",
-         status: "true",
-         vertreter: "J",
-         fachberater: "K",
-         timespan: {
-           start: new Date("2024-02-01"),
-           end: new Date("2024-02-10"),
-         },
-         customer: "I",
-         abschlussbericht: "Nicht gestartet",
-         type: 1,
-       },
-     ];
-   } */
 }
 
 interface DataItem {
-  id?: number;
+  id?: string;
   name?: string;
   dateOfCreation?: Date;
   customerOrCompany?: string;
