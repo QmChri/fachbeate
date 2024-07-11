a37a5cfd5d321bff3f735d8c24f0b84042e0231e
qmraphael <QM.Praktikant1@almi.at>
2024-07-11 15:57:39 +0200

    Keycloak + more Fixes

---
+++ frontend-fachbeate/src/app/models/main-list-dto.ts
@@ -0,0 +1,17 @@
+export interface MainListDTO {
+    id: string;
+    name: string;
+    dateOfCreation: string;
+    customerOrCompany: string;
+    statusGL: string;
+    statusAL: string;
+    representative: string;
+    technologist: string;
+    fromDate: string;
+    toDate: string;
+    customer: string;
+    finalReport: string;
+    type: number;
+    visible: boolean;
+    calendarColor: string;
+}
