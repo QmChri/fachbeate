588d32234381d49c67650f14e9dfc081d504d189
qmraphael <QM.Praktikant1@almi.at>
2024-09-16 09:33:33 +0200

    fullcalendar fix and <Leer> - <Leer> fix

--- frontend-fachbeate/src/environments/environment.development.ts
+++ frontend-fachbeate/src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  backendApi: "http://localhost:8079/",
-  //backendApi: "https://requesttool.almi.at/api",
+  //backendApi: "http://localhost:8079/",
+  backendApi: "https://requesttool.almi.at/api",
   keycloak: {
     issuer: 'https://requesttool.almi.at/',
     realm: 'fachbeate',
